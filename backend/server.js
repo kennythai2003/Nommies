ae71b3952ab92c5e9c15c19c49f71361846795f8
Dilan Guerrero <dilanguerrero@ucr-secure-35-10-13-189-247.wnet.ucr.edu>
2024-06-08 17:46:15 -0700

    fixed search bar

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const { signUp, logIn, googleLogin } = require("./handlers/users")
 connectDB()
 
 const reviewsHandler = require('./handlers/review');
+const restaurantHandler = require('./handlers/restaurant');
 app.use(express.json({ extended: false }))
 
 const cors = require("cors")
@@ -19,8 +20,10 @@ app.post("/googleLogin", googleLogin)
 
 app.get('/getReviews', reviewsHandler.getReviews);
 app.post('/writeReview', reviewsHandler.writeReview);
+app.get('/getRestaurants', restaurantHandler.getRestaurants);
 
-const port = process.env.PORT || 8081
+const port = 8080;
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`)
 })
+
