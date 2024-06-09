8b036c8a2e22860c63b27db07f7920a582c75a2f
Davis Kim <daveykim2011@hotmail.com>
2024-06-08 17:16:04 -0700

    Fixing some styling issues with boostrap

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.post("/googleLogin", googleLogin)
 app.get('/getReviews', reviewsHandler.getReviews);
 app.post('/writeReview', reviewsHandler.writeReview);
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`)
 })
